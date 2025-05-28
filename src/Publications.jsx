942a13cf6d3636db716c99fc81ad7393a0818c41
jaeyoungchoi1 <choijy5847@naver.com>
2025-05-29 00:51:04 +0900

    fix: publications update

--- src/Publications.jsx
+++ src/Publications.jsx
@@ -2,6 +2,67 @@
 export default (
         <section id="publications">
           <h2>Publications</h2>
+          <h3>2025</h3>
+          <ul className="publications">
+            <li>
+              <div className="bib-item">
+                <div className="title">
+                WrightHere: Supporting Children's Creative Writing with AI-Infused Interactive 3D Environment
+                </div>
+                <div className="authors">
+                <em>Jaeryung Chung</em>, <em>Seon Gyeom Kim</em>, and <em>Tak Yeon Lee</em>
+                </div>
+                <div className="description">
+                CHI EA '25: Proceedings of the Extended Abstracts of the CHI Conference on Human Factors in Computing Systems
+                <a className="link" target="_blank" href="https://dl.acm.org/doi/abs/10.1145/3706599.3719932">[Extended Abstract]</a>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="bib-item">
+                <div className="title">
+                LIGS: Developing an LLM-infused Game System for Emergent Narrative
+                </div>
+                <div className="authors">
+                <em>Jin Jeong</em>, and <em>Tak Yeon Lee</em>
+                </div>
+                <div className="description">
+                CHI EA '25: Proceedings of the Extended Abstracts of the CHI Conference on Human Factors in Computing Systems
+                <a className="link" target="_blank" href="https://dl.acm.org/doi/abs/10.1145/3706599.3720212">[Extended Abstract]</a>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="bib-item">
+                <div className="title">
+                Bridging Bond Beyond Life: Designing VR Memorial Space with Stakeholder Collaboration via Research through Design
+                </div>
+                <div className="authors">
+                Heejae Bae, Nayeong Kim, Sehee Lee, and <em>Tak Yeon Lee</em>
+                </div>
+                <div className="description">
+                CHI EA '25: Proceedings of the Extended Abstracts of the CHI Conference on Human Factors in Computing Systems
+                <a className="link" target="_blank" href="https://dl.acm.org/doi/abs/10.1145/3706599.3719871">[Extended Abstract]</a>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="bib-item">
+                <div className="title">
+                Chart-to-Experience: Benchmarking Multimodal LLMs for Predicting Experiential Impact of Charts
+                </div>
+                <div className="authors">
+                <em>Seon Gyeom Kim</em>, <em>Jae Young Choi</em>, Ryan Rossi, Eunyee Koh, and <em>Tak Yeon Lee</em>
+                </div>
+                <div className="description">
+                2025 IEEE 17th Pacific Visualization Conference (PacificVis)
+                <span className="acknowledgement">Best Paper</span>
+                <a className="link" target="_blank" href="https://www.arxiv.org/abs/2505.17374">[Short Paper]</a>
+                </div>
+              </div>
+            </li>
+          </ul>
+          
           <h3>2024</h3>
           <ul className="publications">
             <li>
@@ -10,10 +71,11 @@ export default (
                 RECIPE4U: Student-ChatGPT Interaction Dataset in EFL Writing Education
                 </div>
                 <div className="authors">
-                Jieun Han, Haneul Yoo, Junho Myung, Minsun Kim, Tak Yeon Lee, So-Yeon Ahn and Alice Oh
+                Jieun Han, Haneul Yoo, Junho Myung, <em>Minsun Kim</em>, <em>Tak Yeon Lee</em>, So-Yeon Ahn and Alice Oh
                 </div>
                 <div className="description">
-                Accepted for LREC-COLING 2024
+                Proceedings of the 2024 Joint International Conference on Computational Linguistics, Language Resources and Evaluation
+                <a className="link" target="_blank" href="https://aclanthology.org/2024.lrec-main.1193/">[Full Paper]</a>
                 </div>
               </div>
             </li>
@@ -24,10 +86,11 @@ export default (
                 Virfie: Enhancing Remote Togetherness with User-Created Scenarios for Virtual Group Selfie
                 </div>
                 <div className="authors">
-                Hyerin Im, Taewan Kim, Eunhee Jung, Bonhee Ku, Seungho Baek, Youn-kyung Lim, Tek-Jin Nam, and Tak Yeon Lee
+                Hyerin Im, Taewan Kim, Eunhee Jung, Bonhee Ku, Seungho Baek, Youn-kyung Lim, Tek-Jin Nam, and <em>Tak Yeon Lee</em>
                 </div>
                 <div className="description">
-                Accepted for Intelligent Systems Conference (IntelliSys) 2024, published in Lecture Notes in Networks and Systems
+                Intelligent Systems Conference (IntelliSys) 2024, published in Lecture Notes in Networks and Systems
+                <a className="link" target="_blank" href="https://link.springer.com/chapter/10.1007/978-3-031-66336-9_44">[Full Paper]</a>
                 </div>
               </div>
             </li>
@@ -41,7 +104,7 @@ export default (
                 GUIDE for GAIED: Exploring Student-ChatGPT Dialogue in EFL Writing Education
                 </div>
                 <div className="authors">
-                Jieun Han, Haneul Yoo, Junho Myung, Minsun Kim, Tak Yeon Lee, So-Yeon Ahn, and Alice Oh
+                Jieun Han, Haneul Yoo, Junho Myung, Minsun Kim, <em>Tak Yeon Lee</em>, So-Yeon Ahn, and Alice Oh
                 </div>
                 <div className="description">
                 GAIED Workshop at NeurIPS 2023
@@ -56,7 +119,7 @@ export default (
                 Visual Insight Recommendation: From Ranking Insight Visualizations to Insight Types
                 </div>
                 <div className="authors">
-                Camille Harris, Ryan Rossi, Sana Malik, Jane Hoffswell, Fan Du, Tak Yeon Lee, Eunyee Koh, and Handong Zhao
+                Camille Harris, Ryan Rossi, Sana Malik, Jane Hoffswell, Fan Du, <em>Tak Yeon Lee</em>, Eunyee Koh, and Handong Zhao
                 </div>
                 <div className="description">
                 2023 IEEE International Conference on Big Data Industry and Government Program
@@ -70,7 +133,7 @@ export default (
                 SpotLight: Visual Insight Recommendation
                 </div>
                 <div className="authors">
-                Camille Harris, Ryan Rossi, Sana Malik, Jane Hoffswell, Fan Du, Tak Yeon Lee, Eunyee Koh, and Handong Zhao.
+                Camille Harris, Ryan Rossi, Sana Malik, Jane Hoffswell, Fan Du, <em>Tak Yeon Lee</em>, Eunyee Koh, and Handong Zhao.
                 </div>
                 <div className="description">
                 WWW '23 Companion: Companion Proceedings of the ACM Web Conference 2023
@@ -85,7 +148,7 @@ export default (
                 RECIPE: How to Integrate ChatGPT into EFL Writing Education
                 </div>
                 <div className="authors">
-                Jieun Han, Haneul Yoo, Yoonsu Kim, Junho Myung, Minsun Kim, Hyunseung Lim, Juho Kim, Tak Yeon Lee, Hwajung Hong, So-Yeon Ahn, and Alice Oh
+                Jieun Han, Haneul Yoo, Yoonsu Kim, Junho Myung, Minsun Kim, Hyunseung Lim, Juho Kim, <em>Tak Yeon Lee</em>, Hwajung Hong, So-Yeon Ahn, and Alice Oh
                 </div>
                 <div className="description">
                 L@S '23: Proceedings of the Tenth ACM Conference on Learning @ Scale
@@ -106,7 +169,7 @@ export default (
                 Personalized visualization recommendation
                 </div>
                 <div className="authors">
-                Xin Qian, Ryan A Rossi, Fan Du, Sungchul Kim, Eunyee Koh, Sana Malik, Tak Yeon Lee, Nesreen K Ahmed
+                Xin Qian, Ryan A Rossi, Fan Du, Sungchul Kim, Eunyee Koh, Sana Malik, <em>Tak Yeon Lee</em>, Nesreen K Ahmed
                 </div>
                 <div className="description">
                 ACM Transactions on the Web (TWEB)
@@ -121,7 +184,7 @@ export default (
                 Is It Really Useful?: An Observation Study of How Designers Use CLIP-based Image Generation For Moodboards
                 </div>
                 <div className="authors">
-                Seungho Baek, Hyerin Im, Uran Oh, Youn-kyung Lim, and Tak Yeon Lee
+                Seungho Baek, Hyerin Im, Uran Oh, Youn-kyung Lim, and <em>Tak Yeon Lee</em>
                 </div>
                 <div className="description">
                 HCAI Workshop at NeurIPS 2022
@@ -136,7 +199,7 @@ export default (
                 Virfie: Virtual Group Selfie Station for Remote Togetherness
                 </div>
                 <div className="authors">
-                Hyerin Im, Taewan Kim, Eunhee Jung, Bonhee Ku, Seungho Baek, and Tak Yeon Lee
+                Hyerin Im, Taewan Kim, Eunhee Jung, Bonhee Ku, Seungho Baek, and <em>Tak Yeon Lee</em>
                 </div>
                 <div className="description">
                 In Extended Abstracts of the 2022 CHI Conference on Human Factors in Computing Systems (CHI EA '22)
@@ -244,5 +307,5 @@ export default (
           </ul>
           
           <h3>Prior to 2021</h3>
-            <p>Check Tak Yeon Lee's <a href="https://scholar.google.com/citations?hl=en&user=k2sjjrwAAAAJ" target="_blank">Google Scholar page</a> for publications before 2021</p>
+            <p>Check <em>Tak Yeon Lee</em>'s <a href="https://scholar.google.com/citations?hl=en&user=k2sjjrwAAAAJ" target="_blank">Google Scholar page</a> for publications before 2021</p>
         </section>);
